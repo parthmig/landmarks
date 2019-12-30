54d1ea8071cd905f0d44bf772fab1848f6b46f0c
Parth Miglani <parthmiglani08@gmail.com>
2019-12-30 17:05:35 -0500

    add first landmark text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+ 
+            HStack {
+                Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    .padding()
+        
     }
 }
 
