5fe4e28b247705c83b92218c9f50a5c1ce66ba09
Parth Miglani <pmiglani@uoguelph.ca>
2019-12-30 15:02:49 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Parth Miglani on 2019-12-30.
+//  Copyright © 2019 Parth Miglani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
