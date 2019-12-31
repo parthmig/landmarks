3cac39357f562473ff7aed2278b77ec1ad44ae4e
Parth Miglani <parthmiglani08@gmail.com>
2019-12-30 23:16:17 -0500

    update list view and fix for detail display

--- Landmarks/Landmarks/Supporting Views/MapView.swift
+++ Landmarks/Landmarks/Supporting Views/MapView.swift
@@ -6,13 +6,15 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
 
     func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011_286, longitude: -116.166_868)
+
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
@@ -21,6 +23,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
